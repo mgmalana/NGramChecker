72cb05e3b9d2867c618e1e3bcd652a5a00a476bd
Matthew Go <matthew.go123@gmail.com>
2016-06-17 09:43:55 +0800

    Added wordposlemmamap

--- src/optimization/training/dao/WordPOSLemmaMapDao.java
+++ src/optimization/training/dao/WordPOSLemmaMapDao.java
@@ -2,4 +2,9 @@ package optimization.training.dao;
 
 public class WordPOSLemmaMapDao {
 
+	String wplMapDao = "wordposlemmamap";
+
+	public void addWPLMapping(String word, String pos, String lemma) {
+
+	}
 }
