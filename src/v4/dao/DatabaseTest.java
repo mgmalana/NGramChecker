20fd4f0c2498336dd07c725b4b8cfbd2834acb24
mgmalana <mgmalana@gmail.com>
2017-03-02 17:10:47 +0800

    Added remaining method testers

--- src/v4/dao/DatabaseTest.java
+++ src/v4/dao/DatabaseTest.java
@@ -3,6 +3,9 @@ package v4.dao;
 import junit.framework.TestCase;
 import v4.models.NGram;
 
+import java.util.HashMap;
+import java.util.List;
+
 /**
  * Created by mgmalana on 3/1/17.
  */
@@ -13,6 +16,11 @@ public class DatabaseTest extends TestCase {
     public void testGetConnection() throws Exception {
         DatabaseConnector.getConnection();
     }
+
+    /**
+     *The following are tests are NGramDao
+     *
+     */
 
     public void testClearDatabase() throws Exception {
         nGramDao.clearDatabase();
@@ -29,15 +37,34 @@ public class DatabaseTest extends TestCase {
 
     public void testGet() throws Exception {
         NGram ngram = nGramDao.get(21192);
-        //TODO: Ask matthew bakit yung posid 0
+        //TODO: Ask matthew if pwede ba talga 0 ang posid
     }
 
     public void testIncrementPOSFrequency() throws Exception {
-        nGramDao.incrementPOSFrequency("123456789");
+        nGramDao.incrementPOSFrequency("lalallala");
     }
 
-    public void testCloseConnection() throws Exception {
+    public void testGetPOSFreqID() throws Exception {
+        System.out.println(nGramDao.getPOSFreqID("lalallala"));
     }
 
+    public void testGetSimilarNGrams() throws Exception {
+        List ngrams = nGramDao.getSimilarNGrams(0, 1);
+    }
 
+    public void testGetGeneralizedNGrams() throws Exception {
+        HashMap<Integer, NGram> ngrams = nGramDao.getGeneralizedNGrams();
+    }
+
+    public void testSetIsPOSGeneralized() throws Exception {
+        nGramDao.setIsPOSGeneralized(155494, "true");
+    }
+
+    public void testGetPOS() throws Exception {
+        String[] POS = nGramDao.getPOS(38857);
+    }
+
+    public void testSetIsPOSGeneralizedBatch() throws Exception {
+        //looks like this is working
+    }
 }
